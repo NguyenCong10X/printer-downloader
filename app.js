a79ee93bbfd7186f301e3794760b27dd210dc4a9
NguyenCong04 <nguyentatcong2004@gmail.com>
2024-09-06 15:25:24 +0700

    update language

--- app.js
+++ app.js
@@ -121,7 +121,7 @@ i18n.configure({
 const interval = 5 * 60 * 1000; // 5 phút
 
 setInterval(() => {
-    fetch('https://snapthreadsdownloader.onrender.com')
+    fetch('https://pintersnap.onrender.com')
         .then(res => res.text())
         .then(text => console.log('Ping thành công:', text))
         .catch(err => console.error('Lỗi khi ping:', err));
